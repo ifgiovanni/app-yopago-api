472c6a95e968a49f5aa3a859ffbd3e28f967a613
ifgiovanni <contactargiovanni@gmail.com>
2024-06-30 03:02:52 -0600

    added store deposit option fn

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -28,5 +28,44 @@ class DepositController extends Controller
         return response()->json($data);
     }
 
+    public function store(Request $request){
+        
+        $user = auth()->user();
+        
+        $type = $request->input('type');
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $account_number = $request->input('account_number');
+        $account_name = $request->input('account_name');
+        $account_type = $request->input('account_type');
+        $description = $request->input('description');
+        $details = $request->input('details');
+        $status = $request->input('status');
+        $validation_field = $request->input('validation_field');
+
+        // make json with account_number, account_name, account_type, description, details
+        $options = [
+            'account_number' => $account_number,
+            'account_name' => $account_name,
+            'account_type' => $account_type,
+            'description' => $description,
+            'details' => $details
+        ];
+
+        $deposit_option = new DepositOption();
+        $deposit_option->name = $name;
+        $deposit_option->type = $type;
+        $deposit_option->description = $description;
+        $deposit_option->options = json_encode($options);
+        $deposit_option->status = $status;
+        $deposit_option->validation_field = $validation_field;
+        $deposit_option->save();
+
+        $data = [
+            'message' => 'Deposit option created successfully'
+        ];
+    
+        return response()->json($data);
+    }
 
 }
